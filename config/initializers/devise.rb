d6e1d3348fb9fa82fdb64027b866ae56f4172cc4
123 <cookie.twenty01@gmail.com>
2021-04-15 14:49:38 -0400

    render#3

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,6 +2,9 @@ Devise.setup do |config|
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
   require 'devise/orm/active_record'
+  config.case_insensitive_keys = [:username]
+
+  config.strip_whitespace_keys = [:username]
 
   config.case_insensitive_keys = [:email]
 
