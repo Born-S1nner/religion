0b8aa47391fc0e96da9704c6cef25925f6abad57
123 <cookie.twenty01@gmail.com>
2021-04-17 13:03:11 -0400

    change

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ApplicationRecord
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         has_many :peers 
-         has_many :messages
+  has_many :peers 
+  has_many :messages
+  
+  after_create :notify_pusher
+
+  def notify_pusher
+    Pusher.trigger('activity', 'login', self.as_json)
+  end
+   
+  def as_json(options={}) # add this method
+    super(
+      only: [:id, :email, :username, :religion]
+    )
+  end
 end
